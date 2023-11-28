7711c7787efaafac460d0bcf95add7e04e60f63d
Van Eliot Santos <van.eliot.santos@gmail.com>
2023-11-28 12:03:30 +0800

    Setup connection between knex and pg db; Add example code

--- app.js
+++ app.js
@@ -12,9 +12,37 @@ app.use(express.json());
 // Middleware
 // ...
 
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+app.post('/insert-user', async (req, res) => {
+  // TODO: Implement
+
+  // Example code
+  /*
+  const result = await db('users')
+    .insert({
+      first_name: 'foo',
+      last_name: 'bar',
+    })
+    .returning('*');
+  console.log({ result });
+
+  res.json(result);
+  */
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
