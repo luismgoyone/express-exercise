e809755505e5032744b1acac6d6c01423213bdd3
Van Eliot Santos <van.eliot.santos@gmail.com>
2023-11-28 13:43:28 +0800

    Implement handler for user creation

--- app.js
+++ app.js
@@ -27,21 +27,50 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-app.post('/insert-user', async (req, res) => {
-  // TODO: Implement
+app.post('/create-user', async (req, res) => {
+  const {
+    first_name,
+    last_name,
+  } = req.body;
 
-  // Example code
-  /*
+  const errors = [];
+
+  // TODO: Maybe refactor using express-validator
+  if (!first_name) {
+    errors.push({ message: 'Parsed falsy value for param "first_name"' });
+  } else if (typeof last_name !== 'string') {
+    errors.push({ message: 'Value for param "first_name" should be of type "string"' });
+  } else if (
+    !first_name.length ||
+    first_name.length > 20
+  ) {
+    errors.push({ message: 'String "first_name" should have a length of 1 up to 20 characters' });
+  }
+
+  if (!last_name) {
+    errors.push({ message: 'Parsed falsy value for param "last_name"' });
+  } else if (typeof last_name !== 'string') {
+    errors.push({ message: 'Value for param "last_name" should be of type "string"' });
+  } else if (
+    !last_name.length ||
+    last_name.length > 20
+  ) {
+    errors.push({ message: 'String "last_name" should have a length of 1 up to 20 characters' });
+  }
+
+  if (errors.length) {
+    res.status(400).json({ errors });
+    return;
+  }
+
   const result = await db('users')
     .insert({
-      first_name: 'foo',
-      last_name: 'bar',
+      first_name: first_name.trim(),
+      last_name: last_name.trim(),
     })
     .returning('*');
-  console.log({ result });
 
   res.json(result);
-  */
 });
 
 app.listen(port, () => {
