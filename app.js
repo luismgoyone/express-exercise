b96fa6003566c8a13188ced3fefaf32aabf40c7c
Van Eliot Santos <van.eliot.santos@gmail.com>
2023-11-28 14:08:15 +0800

    Create router for users
    Create router for users; Relocate handler for user creation; Rename path for user creation

--- app.js
+++ app.js
@@ -14,54 +14,10 @@ const db = require('./db/dbClient');
 // Middleware
 // ...
 
+app.use('/users', require('./routes/users'));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
-});
-
-app.post('/create-user', async (req, res) => {
-  const {
-    first_name,
-    last_name,
-  } = req.body;
-
-  const errors = [];
-
-  // TODO: Maybe refactor using express-validator
-  if (!first_name) {
-    errors.push({ message: 'Parsed falsy value for param "first_name"' });
-  } else if (typeof last_name !== 'string') {
-    errors.push({ message: 'Value for param "first_name" should be of type "string"' });
-  } else if (
-    !first_name.length ||
-    first_name.length > 20
-  ) {
-    errors.push({ message: 'String "first_name" should have a length of 1 up to 20 characters' });
-  }
-
-  if (!last_name) {
-    errors.push({ message: 'Parsed falsy value for param "last_name"' });
-  } else if (typeof last_name !== 'string') {
-    errors.push({ message: 'Value for param "last_name" should be of type "string"' });
-  } else if (
-    !last_name.length ||
-    last_name.length > 20
-  ) {
-    errors.push({ message: 'String "last_name" should have a length of 1 up to 20 characters' });
-  }
-
-  if (errors.length) {
-    res.status(400).json({ errors });
-    return;
-  }
-
-  const result = await db('users')
-    .insert({
-      first_name: first_name.trim(),
-      last_name: last_name.trim(),
-    })
-    .returning('*');
-
-  res.json(result);
 });
 
 app.listen(port, () => {
