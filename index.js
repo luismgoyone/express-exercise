64800904a6cced68be77e135767875bd15d1faea
Luis Goyone <luismichaelgoyone@gmail.com>
2023-11-28 17:48:51 +0800

    Add routes in index

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const userRoutes = require('./src/users/routes');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.send('Hello World');
 });
+
+app.use('/api/v1/users', userRoutes);
+
 app.listen(port, () => {
   console.log('server started');
 });
