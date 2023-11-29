e986eaf6bf3d964e626818f17851567e61c1cd11
Adrian Immanuel Ilaga <adrianilaga771@gmail.com>
2023-11-29 10:43:58 +0800

    initiate project

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = os.Getenv("DBPASS")
+	dbname   = os.Getenv("DBNAME")
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	println("Connection Established!")
+}
