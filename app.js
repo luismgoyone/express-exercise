0b9c63cbc9119f77411dd9534dea32c8a75558f7
Van Eliot Santos <van.eliot.santos@gmail.com>
2023-11-30 16:05:48 +0800

    Add example code for creating a table using knex.schema

--- app.js
+++ app.js
@@ -20,6 +20,41 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
+app.get('/posts', (req, res) => {
+  // WIP
+  let error = null;
+
+  // NOTE: Just an example code for creating a table via the knex.schema API
+  // TODO: Relocate to a JS file for schema-related functionalities
+  knex.schema.hasTable('posts')
+    .then((exists) => {
+      if (exists) {
+        console.info('table `posts` already exists');
+        return;
+      }
+
+      return knex.schema.createTable('posts', (t) => {
+        t.integer('user_id').unsigned().references('id').inTable('users');
+        t.text('content').defaultTo(null);
+        t.timestamp('created_at', knex.fn.now());
+      });
+    })
+    .catch((err) => {
+      const message = 'Error creating table `posts`:' + err.message;
+      console.error(message);
+      error = { message };
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+
+  if (error) {
+    return res.status(500).json(error);
+  }
+
+  res.json({ message: '`posts` table now ready' });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
