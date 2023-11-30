fe555aa86c92c7863eb1151f1f22bc52c1d484ee
Keemchard <keemchardtamio@gmail.com>
2023-11-30 15:50:39 +0800

    add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from "express";
 import pool from "./db";
+import crypto from "crypto";
 
 export function setupRoutes(app: Express): void {
   // Register user
@@ -34,10 +35,11 @@ export function setupRoutes(app: Express): void {
 
       const userId = userResult.rows[0].id;
 
+      const generatedToken = crypto.randomBytes(32).toString("hex");
       // Insert into user_logins table
       const loginResult = await pool.query(
         "INSERT INTO user_logins (user_id, token, last_login_at, username, password) VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4) RETURNING user_id, token, username",
-        [userId, "your_generated_token", username, password]
+        [userId, generatedToken, username, password]
       );
 
       const userRecord = {
@@ -49,6 +51,51 @@ export function setupRoutes(app: Express): void {
 
       console.log("success hehe!");
       return res.status(201).json(userRecord);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  // -----------------------------------------------------------------
+
+  // Login user
+  app.post("/login", async (req: Request, res: Response) => {
+    try {
+      const { username, password } = req.body;
+
+      // Check if the username exists
+      const userResult = await pool.query(
+        "SELECT * FROM user_logins WHERE username = $1",
+        [username]
+      );
+
+      if (userResult.rows.length === 0) {
+        return res.status(400).json({ error: "Invalid username or password." });
+      }
+
+      const userId = userResult.rows[0].id;
+
+      // Check if the username and password combination is valid
+      const loginResult = await pool.query(
+        "SELECT * FROM user_logins WHERE username = $1 AND password = $2",
+        [username, password]
+      );
+
+      if (loginResult.rows.length === 0) {
+        return res.status(400).json({ error: "Invalid username or password." });
+      }
+
+      const userRecord = {
+        id: userId,
+        first_name: userResult.rows[0].first_name,
+        last_name: userResult.rows[0].last_name,
+        username: username,
+        token: loginResult.rows[0].token,
+      };
+
+      console.log("login success!");
+      return res.status(200).json(userRecord);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Internal Server Error" });
