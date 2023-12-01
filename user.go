986c5df876e75dbfafd140df3acba85e040cfcdb
Adrian Immanuel Ilaga <adrianilaga771@gmail.com>
2023-12-01 11:43:54 +0800

    add insert user function

--- user.go
+++ user.go
@@ -2,6 +2,20 @@ package main
 
 // CREATE
 
+func insertUser(user User) (int, error) {
+	sqlStatement := `
+	INSERT INTO users (first_name, last_name)
+	VALUES ($1, $2)
+	RETURNING id`
+	var id int
+	row := db.QueryRow(sqlStatement, user.first_name, user.last_name)
+	err := row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	return id, err
+}
+
 // READ
 
 // UPDATE
