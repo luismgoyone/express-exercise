bffacf0f27c5cc371677f00b5001c7be781c3ec9
Adrian Immanuel Ilaga <adrianilaga771@gmail.com>
2023-12-04 10:42:56 +0800

    add insertUserLogin function

--- userLogin.go
+++ userLogin.go
@@ -2,6 +2,20 @@ package main
 
 // CREATE
 
+func insertUserLogin(userLogin UserLogin) (int, error) {
+	sqlStatement := `
+	INSERT INTO user_logins (user_id, username, password)
+	VALUES ($1, $2, $3)
+	RETURNING user_id`
+	var user_id int
+	row := db.QueryRow(sqlStatement, userLogin.user_id, userLogin.username, userLogin.password)
+	err := row.Scan(&user_id)
+	if err != nil {
+		panic(err)
+	}
+	return user_id, err
+}
+
 // READ
 
 // UPDATE
