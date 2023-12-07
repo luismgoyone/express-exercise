883879ddabdfc620316df3339b8cc28a9388be92
Luis Goyone <luismichaelgoyone@gmail.com>
2023-12-07 21:32:33 +0800

    Cleanup code

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('Hello World');
-});
-
 app.use('/api/v1', userRoutes);
 
 app.listen(port, () => {
