43e3bad83b4dd2ab450cdf3c5ea84d169d703e6f
Luis Goyone <luismichaelgoyone@gmail.com>
2023-12-11 14:56:11 +0800

    Cleanup code

--- src/users/queries.js
+++ src/users/queries.js
@@ -1,5 +1,3 @@
-const getUsers = 'SELECT * FROM users';
-const registerUser = '';
 const validateUsername = 'SELECT u FROM user_logins u WHERE u.username = $1';
 const insertUserToUsersTable =
   'INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING id';
@@ -9,14 +7,15 @@ const getRegisteredUser =
   'SELECT users.id, users.first_name, users.last_name, user_logins.username FROM users INNER JOIN user_logins ON users.id = user_logins.user_id WHERE users.id = $1';
 const validateUser =
   'SELECT * FROM user_logins u WHERE u.username = $1 AND u.password = $2';
+const getUsers = 'SELECT * FROM users';
 const getLoggedInUser =
   'SELECT u.id, u.first_name, u.last_name, ul.username, ul.token FROM users u INNER JOIN user_logins ul ON u.id = ul.user_id WHERE u.id = $1';
 module.exports = {
-  getUsers,
   validateUsername,
   insertUserToUsersTable,
   insertUserToUserLoginsTable,
   getRegisteredUser,
   validateUser,
+  getUsers,
   getLoggedInUser,
 };
