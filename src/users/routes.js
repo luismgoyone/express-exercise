79097131d44ae82200498305063d195cc564523c
Luis Goyone <luismichaelgoyone@gmail.com>
2023-12-11 16:40:57 +0800

    Add logoutUser route

--- src/users/routes.js
+++ src/users/routes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/', controller.getUsers);
 router.post('/register', controller.registerUser);
 router.post('/login', controller.loginUser);
+router.post('/logout', controller.logoutUser);
 
 module.exports = router;
