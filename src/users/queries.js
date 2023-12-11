56ce3fcd250afbf614cb9a02a8157dac368fdb83
Luis Goyone <luismichaelgoyone@gmail.com>
2023-12-11 14:55:16 +0800

    Add query for validating username and password

--- src/users/queries.js
+++ src/users/queries.js
@@ -7,10 +7,13 @@ const insertUserToUserLoginsTable =
   'INSERT INTO user_logins (user_id, token, last_login_at, username, password) VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4)';
 const getRegisteredUser =
   'SELECT users.id, users.first_name, users.last_name, user_logins.username FROM users INNER JOIN user_logins ON users.id = user_logins.user_id WHERE users.id = $1';
+const validateUser =
+  'SELECT * FROM user_logins u WHERE u.username = $1 AND u.password = $2';
 module.exports = {
   getUsers,
   validateUsername,
   insertUserToUsersTable,
   insertUserToUserLoginsTable,
   getRegisteredUser,
+  validateUser,
 };
