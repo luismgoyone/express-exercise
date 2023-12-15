f51f181124e4dd2b33e9714a898154bad44c091d
Van Eliot Santos <van.eliot.santos@gmail.com>
2023-12-15 12:43:40 +0800

    Create test for /posts/all with mock JWT

--- routes/posts.test.js
+++ routes/posts.test.js
@@ -5,3 +5,39 @@ const {
 } = require('@jest/globals');
 const request = require('supertest');
 const app = require('../app');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const mockToken = jwt.sign(
+  {
+    username: 'mockusername',
+    user_id: '0',
+  },
+  process.env.AUTH_SECRET
+);
+
+describe('GET /posts/all', () => {
+  test('given a valid token', async () => {
+    const response = await request(app)
+      .get('/posts/all')
+      .set('Authorization', `Bearer ${mockToken}`);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('given an invalid token', async () => {
+    const invalidToken = jwt.sign(
+      {
+        username: 'mockusername',
+        user_id: '0',
+      },
+      'wrong_secret'
+    );
+
+    const response = await request(app)
+      .get('/posts/all')
+      .set('Authorization', `Bearer ${invalidToken}`);
+
+    expect(response.statusCode).toBe(401);
+  });
+});
