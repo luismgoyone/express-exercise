75b41813da0c1aedc686554f6e7e680142b55f48
Luis Goyone <luismichaelgoyone@gmail.com>
2024-01-01 19:12:03 +0800

    Add postRoutes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const userRoutes = require('./src/users/routes');
+const postRoutes = require('./src/posts/routes');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.use('/api/v1/users', userRoutes);
+
+app.use('/api/v1/posts', postRoutes);
 
 app.listen(port, () => {
   console.log('server started');
